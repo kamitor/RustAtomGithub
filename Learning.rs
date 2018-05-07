f57aa7a7a0e71951e5c074457f7c03abbee86e15
kamitor <christiaan_gerardo@hotmail.com>
2018-05-07 22:47:23 +0200

    now with compile :D ohw god so many errors! This :D hahahah this compiler :D I LOVE THIS COMPILER!

--- Learning.rs
+++ Learning.rs
@@ -2,5 +2,11 @@ fn main () {
 
 
     println!("Hello everyone, is this working?");
-    println!("This is hard sometimes :P");
+    println!("{0}, {1}!", "Hello", "chris");
+    println!("{greetings}, {name}!", greetings="Hallo", name="christiaan");
+    println!("{:?}", [1,2,3]);
+    println!("{:#?}", [1,2,3]);
+    let y = format!("{}, {}!", "Hai", "Kamitor");
+    println!("{}", y);
+
 }
